94b96921662c2830d0f8edc7f9a3426dd1238153
Facundo Brun <124920888+facubrun@users.noreply.github.com>
2025-04-18 12:44:35 -0300

    test

--- desktop.py
+++
@@ -1,8 +0,0 @@
-import PySimpleGUI as sg
-
-
-layout = [[sg.Text("Hola"), sg.Input()],
-          [sg.Text("Mundo")],
-          [sg.Button("Ok")]]
-
-window = sg.Window("Demo", layout, margins=(100,100)).read()
