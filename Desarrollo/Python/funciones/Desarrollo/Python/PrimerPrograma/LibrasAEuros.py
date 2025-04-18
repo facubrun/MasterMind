f1201bb04e58a36d946eaedc1212170c6cae5bd0
Facundo Brun <124920888+facubrun@users.noreply.github.com>
2025-04-18 13:00:44 -0300

    Delete Desarrollo/Python/funciones/Desarrollo/Python directory

--- Desarrollo/Python/funciones/Desarrollo/Python/PrimerPrograma/LibrasAEuros.py
+++
@@ -1,4 +0,0 @@
-libras = float(input("Ingrese la cantidad de libras para hacer la conversión: "))
-euros = libras * 1.15
-print(str(libras) + " libras son "
-      + str(euros) + " euros")
--- Desarrollo/Python/funciones/Desarrollo/Python/PrimerPrograma/for_ej3.py
+++
@@ -1,6 +0,0 @@
-
-num_elegido = int(input("Introduzca el numero a multiplicar:"))
-
-for n in range(1,11):
-    if n % 2 == 0: #Devuelve solo la multiplicacion con multiplos de 2
-        print("{} x {} = {}".format(num_elegido, n, num_elegido * n))
