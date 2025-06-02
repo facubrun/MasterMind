5b2997f2dad925424ee82e9ebc533e6292c634f6
Facundo Brun <facundobrun0303@gmail.com>
2025-06-01 22:38:56 -0300

    desa merge

--- Desarrollo/Python/funciones/exe/setup.py
+++ Desarrollo/Python/funciones/exe/setup.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from distutils.core import setup
 import py2exe
 import glob
@@ -9,16 +8,4 @@ from pathlib import Path
 from time import sleep
 from random import randrange
 
-=======
-from distutils.core import setup
-import py2exe
-import glob
-import os
-import re
-import sqlite3
-from pathlib import Path
-from time import sleep
-from random import randrange
-
->>>>>>> 94b96921662c2830d0f8edc7f9a3426dd1238153
 setup(windows=['hackerscript.py'])
