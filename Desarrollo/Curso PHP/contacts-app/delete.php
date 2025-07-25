7141ec03dea8f08f9112cf6d39b00778302082aa
Facundo Brun <facundobrun0303@gmail.com>
2025-07-25 19:02:56 -0300

    delete contact

---
+++ Desarrollo/Curso PHP/contacts-app/delete.php
@@ -0,0 +1,20 @@
+<?php
+
+require "database.php";
+
+$id = $_GET["id"];
+
+$stmt = $conn->prepare("SELECT * FROM contacts WHERE id = :id");
+$stmt->execute([":id" => $id]);
+
+
+if (stmt->rowCount() == 0) {
+  http_response_code(404);
+  echo "HTTP 404 NOT FOUND";
+  return;
+}
+
+$stmt = $conn->prepare("DELETE FROM contacts WHERE id = :id");
+$stmt->execute([":id" => $id]);
+
+header("Location: index.php");
