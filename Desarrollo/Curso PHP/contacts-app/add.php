8d6ef4900d469a60925b181e4a37b72c33f27520
Facundo <facundobrun0303@gmail.com>
2025-07-25 11:28:14 -0300

    store contacts

--- Desarrollo/Curso PHP/contacts-app/add.php
+++ Desarrollo/Curso PHP/contacts-app/add.php
@@ -1,22 +1,14 @@
 <?php
+  require "database.php";
+
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $contact = [
-      "name" => $_POST["name"],
-      "phone_number" => $_POST["phone_number"],
-    ];
+      $name = $_POST["name"];
+      $phoneNumber = $_POST["phone_number"];
 
+      $stmt = $conn->prepare("INSERT INTO contacts (name, phone_number) VALUES ('$name', '$phoneNumber')");
+      $stmt->execute();
 
-    if (file_exists("contacts.json")) {
-      $contacts = json_decode(file_get_contents("contacts.json"), true);
-    } else {
-      $contacts = [];
-    }
-
-    $contacts[] = $contact;
-
-    file_put_contents("contacts.json", json_encode($contacts));
-
-    header("Location: index.php");
+      header("Location: index.php");
   }
 ?>
 
