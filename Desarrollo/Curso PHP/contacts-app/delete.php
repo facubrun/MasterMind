f8675311b6c3811135fdd4980cb06b24dcb64a45
Facundo Brun <facundobrun0303@gmail.com>
2025-07-26 15:20:51 -0300

    login & logout

--- Desarrollo/Curso PHP/contacts-app/delete.php
+++ Desarrollo/Curso PHP/contacts-app/delete.php
@@ -2,19 +2,22 @@
 
 require "database.php";
 
-$id = $_GET["id"];
-
-$stmt = $conn->prepare("SELECT * FROM contacts WHERE id = :id");
-$stmt->execute([":id" => $id]);
-
-
-if (stmt->rowCount() == 0) {
-  http_response_code(404);
-  echo "HTTP 404 NOT FOUND";
+if (!isset($_SESSION["user"])) {
+  header("Location: login.php");
   return;
 }
 
-$stmt = $conn->prepare("DELETE FROM contacts WHERE id = :id");
-$stmt->execute([":id" => $id]);
+$id = $_GET["id"];
+
+$statement = $conn->prepare("SELECT * FROM contacts WHERE id = :id");
+$statement->execute([":id" => $id]);
+
+if ($statement->rowCount() == 0) {
+  http_response_code(404);
+  echo("HTTP 404 NOT FOUND");
+  return;
+}
+
+$conn->prepare("DELETE FROM contacts WHERE id = :id")->execute([":id" => $id]);
 
 header("Location: home.php");
--- Desarrollo/Curso PHP/contacts-app/edit.php
+++ Desarrollo/Curso PHP/contacts-app/edit.php
@@ -2,6 +2,11 @@
 
   require "database.php";
 
+  if (!isset($_SESSION["user"])) {
+    header("Location: login.php");
+    return;
+  }
+
   $id = $_GET["id"];
 
   $statement = $conn->prepare("SELECT * FROM contacts WHERE id = :id LIMIT 1");
@@ -38,44 +43,46 @@
   }
 ?>
 
-<?php require "partials/headers.php"; ?>
-    <div class="container pt-5">
-      <div class="row justify-content-center">
-        <div class="col-md-8">
-          <div class="card">
-            <div class="card-header">Add New Contact</div>
-            <div class="card-body">
-              <?php if ($error): ?>
-                <p class="text-danger">
-                  <?= $error ?>
-                </p>
-              <?php endif ?>
-              <form method="POST" action="edit.php?id=<?= $contact['id'] ?>">
-                <div class="mb-3 row">
-                  <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
-    
-                  <div class="col-md-6">
-                    <input value="<?= $contact['name'] ?>" id="name" type="text" class="form-control" name="name" autocomplete="name" autofocus>
-                  </div>
-                </div>
-    
-                <div class="mb-3 row">
-                  <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
-    
-                  <div class="col-md-6">
-                    <input value="<?= $contact['phone_number'] ?>" id="phone_number" type="tel" class="form-control" name="phone_number" autocomplete="phone_number" autofocus>
-                  </div>
-                </div>
-    
-                <div class="mb-3 row">
-                  <div class="col-md-6 offset-md-4">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                  </div>
-                </div>
-              </form>
+<?php require "partials/header.php" ?>
+
+<div class="container pt-5">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">Add New Contact</div>
+        <div class="card-body">
+          <?php if ($error): ?>
+            <p class="text-danger">
+              <?= $error ?>
+            </p>
+          <?php endif ?>
+          <form method="POST" action="edit.php?id=<?= $contact['id'] ?>">
+            <div class="mb-3 row">
+              <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
+
+              <div class="col-md-6">
+                <input value="<?= $contact['name'] ?>" id="name" type="text" class="form-control" name="name" autocomplete="name" autofocus>
+              </div>
             </div>
-          </div>
+
+            <div class="mb-3 row">
+              <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
+
+              <div class="col-md-6">
+                <input value="<?= $contact['phone_number'] ?>" id="phone_number" type="tel" class="form-control" name="phone_number" autocomplete="phone_number" autofocus>
+              </div>
+            </div>
+
+            <div class="mb-3 row">
+              <div class="col-md-6 offset-md-4">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
-    <?php require "partials/footer.php"; ?>
+  </div>
+</div>
+
+<?php require "partials/footer.php" ?>
