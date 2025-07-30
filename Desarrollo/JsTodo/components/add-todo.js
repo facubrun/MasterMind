7b44b50bc668ad74c89ec07a90601d0d84df3300
Facundo Brun <facundobrun0303@gmail.com>
2025-07-30 20:15:25 -0300

    curso js terminado

--- Desarrollo/JsTodo/components/add-todo.js
+++ Desarrollo/JsTodo/components/add-todo.js
@@ -1,21 +1,22 @@
-import Alert from "./alert.js";
+import Alert from './alert.js';
 
-export default class addTodo {
-    constructor () {
-        this.btn = document.getElementById('add');
-        this.title = document.getElementById('title');
-        this.description = document.getElementById('description');
-        this.alert = new Alert();
-    }
+export default class AddTodo {
+  constructor() {
+    this.btn = document.getElementById('add');
+    this.title = document.getElementById('title');
+    this.description = document.getElementById('description');
 
-    onClick(callback) {
-        this.btn.onclick = () => {
-            if (title.value === '' || description.value === ''){ // === compara tipo ademas del valor
-                this.alert.show('Title and description are required');
-            } else {
-                this.alert.hide();
-                callback(this.title.value, this.description.value);
-            }
-        } 
+    this.alert = new Alert('alert');
+  }
+
+  onClick(callback) {
+    this.btn.onclick = () => {
+      if (title.value === '' || description.value === '') {
+        this.alert.show('Title and description are required');
+      } else {
+        this.alert.hide();
+        callback(this.title.value, this.description.value);
+      }
     }
+  }
 }
