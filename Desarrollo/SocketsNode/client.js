5cda1ef117607ad1db1835b3791b0d69e59bc34f
Facundo Brun <facundobrun0303@gmail.com>
2025-07-31 19:30:55 -0300

    sockets act

--- Desarrollo/SocketsNode/client.js
+++ Desarrollo/SocketsNode/client.js
@@ -1,25 +1,65 @@
-const { Socket } = require('net');
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
+const { Socket } = require("net");
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-const END = 'END';
+const END = "END";
 
-const socket = new Socket();
+const error = (message) => {
+  console.error(message);
+  process.exit(1);
+};
 
-socket.connect({ host: 'localhost', port: 8000 });
-socket.setEncoding('utf-8');
+const connect = (host, port) => {
+  console.log(`Connecting to ${host}:${port}`);
 
-readline.on("line", (message) => {
-    socket.write(message);
-    if(message === END){
+  const socket = new Socket();
+  socket.connect({ host, port });
+  socket.setEncoding("utf-8");
+
+  socket.on("connect", () => {
+    console.log("Connected");
+
+    readline.question("Choose your username: ", (username) => {
+      socket.write(username);
+      console.log(`Type any message to send it, type ${END} to finish`);
+    });
+
+    readline.on("line", (message) => {
+      socket.write(message);
+      if (message === END) {
         socket.end();
-    }
-});
+      }
+    });
 
-socket.on("data", (data) => {
-    console.log(data);
-});
+    socket.on("data", (data) => {
+      console.log(data);
+    });
+  });
 
-socket.on("close", () => process.exit(0));
+  socket.on("error", (err) => error(err.message));
+
+  socket.on("close", () => {
+    console.log("Disconnected");
+    process.exit(0);
+  });
+};
+
+const main = () => {
+  if (process.argv.length !== 4) {
+    error(`Usage: node ${__filename} host port`);
+  }
+
+  let [, , host, port] = process.argv;
+  if (isNaN(port)) {
+    error(`Invalid port ${port}`);
+  }
+  port = Number(port);
+
+  connect(host, port);
+};
+
+if (module === require.main) {
+  main();
+}
--- Desarrollo/SocketsNode/server.js
+++ Desarrollo/SocketsNode/server.js
@@ -1,65 +1,75 @@
-const { Server } = require('net');
+const { Server } = require("net");
 
 const host = "0.0.0.0";
-const END = 'END';
+const END = "END";
 
 const connections = new Map();
 
 const error = (message) => {
-    console.error(message);
-    process.exit(1);
-}
+  console.error(message);
+  process.exit(1);
+};
 
 const sendMessage = (message, origin) => {
-    // Mandar a todos menos origin el message
-}
+  for (const socket of connections.keys()) {
+    if (socket !== origin) {
+      socket.write(message);
+    }
+  }
+};
 
 const listen = (port) => {
-    const server = new Server();
-    server.on("connection", (socket) => {
-        const remoteSocket = `${socket.remoteAddress}:${socket.remotePort}`;
-        console.log(`New connection from ${remoteSocket}`);
-        socket.setEncoding('utf-8');
-        
-        socket.on("data", (message) => {
-            if(!connections.has(socket)){
-                console.log(`Username ${message} set for connection ${remoteSocket}`);
-                connections.set(socket, message);
-            }else if(message === END){
-                socket.end();
-            }else{
-                // Enviar el message al resto de usuarios
-                console.log(`${remoteSocket} -> ${message}`);
-            }
-            });
-            
-            socket.on("close", () => {
-                console.log(`Connection with ${remoteSocket} closed`)
-            });
-        });
+  const server = new Server();
 
-        server.listen({ port, host }, () =>{
-            console.log(`Listening on port ${port}`)
-        });
+  server.on("connection", (socket) => {
+    const remoteSocket = `${socket.remoteAddress}:${socket.remotePort}`;
+    console.log(`New connection from ${remoteSocket}`);
+    socket.setEncoding("utf-8");
 
-        server.on("error", (err) => error(err.message));
-}
+    socket.on("data", (message) => {
+      connections.values();
+      if (!connections.has(socket)) {
+        console.log(`Username ${message} set for connection ${remoteSocket}`);
+        connections.set(socket, message);
+      } else if (message === END) {
+        connections.delete(socket);
+        socket.end();
+      } else {
+        const fullMessage = `[${connections.get(socket)}]: ${message}`;
+        console.log(`${remoteSocket} -> ${fullMessage}`);
+        sendMessage(fullMessage, socket);
+      }
+    });
+
+    socket.on("error", (err) => console.error(err));
+
+    socket.on("close", () => {
+      console.log(`Connection with ${remoteSocket} closed`);
+    });
+  });
+
+  server.listen({ port, host }, () => {
+    console.log("Listening on port 8000");
+  });
+
+  server.on("error", (err) => error(err.message));
+};
 
 const main = () => {
-    if(process.argv.length !== 3) {
-        error(`Usage: node ${__filename} port`);
-    }
+  if (process.argv.length !== 3) {
+    error(`Usage: node ${__filename} port`);
+  }
 
-    let port = process.argv[2];
-    if(NaN(port)) {
-        error(`Invalid port ${port}`);
-    }
+  let port = process.argv[2];
+  if (isNaN(port)) {
+    error(`Invalid port ${port}`);
+  }
 
-    port = Number(port);
+  port = Number(port);
 
-    listen(port);
-}
+  listen(port);
+};
 
-if( require.main === module){
-        main();
+if (require.main === module) {
+  main();
 }
